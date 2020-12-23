5827c01b3e8e2d3717093b18973819d5d04ecdfe
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-23 13:40:10 +0200

    updated task

--- t05/index.js
+++ t05/index.js
@@ -1,3 +1,4 @@
+//for normal
 
 var normal = require("./normal-router");
 let time = normal().split("-");
@@ -16,3 +17,23 @@ var server = http.createServer(function(req, res) {
 
 console.log("Listening at " + serverUrl + ":" + port);
 server.listen(port, serverUrl);
+
+//for quantum
+
+// var quantum = require("./quantum-router");
+// let time = quantum();
+// var http = require("http");
+// var port = 3000;
+// var serverUrl = "localhost";
+
+// var server = http.createServer(function(req, res) {
+
+//     console.log("Request:" + req.url);
+
+//     var now = new Date();
+//     var html = `<p>In real life you were absent for ${time[0]} years, ${time[1]} months, ${time[2]} days..</p>`
+//     res.end(html);
+// });
+
+// console.log("Listening at " + serverUrl + ":" + port);
+// server.listen(port, serverUrl);
